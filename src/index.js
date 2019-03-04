6a6dccdbd7ba985fedcdfae40c3db2a29b4726d2
gferreira <gferreiraa@gmail.com>
2019-03-03 23:12:38 -0300

    Nunjucks: Config update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ app.get('/', (req, res) => {
   return res.send('Hello World')
 })
 
+app.get('/nome/:name', (req, res) => {
+  return res.send(`Bem-vindo, ${req.params.name}`)
+})
+
+app.get('/outro', (req, res) => {
+  return res.send(`Bem-vindo, ${req.query.name}`)
+})
+
+app.get('/ok/:name', (req, res) => {
+  return res.json({
+    message: `Bem-vindo, ${req.params.name}`
+  })
+})
+
 app.listen(3000)
