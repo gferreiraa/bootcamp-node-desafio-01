aaaf317c20186d91044d76fbd1b25af24f8ca1d9
gferreira <gferreiraa@gmail.com>
2019-03-04 23:46:18 -0300

    Nunjucks: Templates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const app = express()
 
+nunjucks.configure('./views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.set('view engine', 'njk')
+
 app.get('/', (req, res) => {
+  return res.render('index', {
+    name: 'Hello World'
+  })
+})
+
+/* app.get('/', (req, res) => {
   return res.send('Hello World')
 })
 
@@ -17,6 +33,6 @@ app.get('/ok/:name', (req, res) => {
   return res.json({
     message: `Bem-vindo, ${req.params.name}`
   })
-})
+}) */
 
 app.listen(3000)
